782763445547325e46304bb40eeb55f0f3ba7404
n-ogawa <fy59.kou-729@live.jp>
2025-06-15 15:36:54 +0900

    Add EasyCodingStandard for code style enforcement

---
+++ ecs.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+
+    // add a single rule
+    ->withRules([
+        NoUnusedImportsFixer::class,
+    ])
+
+    // add sets - group of rules, from easiest to more complex ones
+    // uncomment one, apply one, commit, PR, merge and repeat
+    ->withPreparedSets(
+        psr12: true,
+    )
+    ;
